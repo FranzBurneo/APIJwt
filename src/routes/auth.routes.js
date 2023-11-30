d9e90a0cad8ef60d5b1a58b639b87f892323bf9a
FranzBurneo <franzdavidburneo@gmail.com>
2023-11-30 00:15:48 -0500

    feat: validate roles and jwt

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,4 +1,10 @@
 import { Router } from "express";
+import * as authCtrl from '../controllers/auth.controller'
+import { verifySignup } from "../middlewares";
 const router = Router()
+
+router.post('/signin', authCtrl.signIn)
+
+router.post('/signup', [verifySignup.checkDuplicateUsernameOrEmail, verifySignup.checkRolesExisted, authCtrl.signUp])
 
 export default router;
